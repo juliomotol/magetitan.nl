e197af82b5d86cee9b94e93c8163fe1509cb9b9f
Julio Motol <julio.motol89@gmail.com>
2025-04-25 17:00:34 +0800

    ✨ Add favicon
    Signed-off-by: Julio Motol <julio.motol89@gmail.com>

--- resources/views/components/layout/base.blade.php
+++ resources/views/components/layout/base.blade.php
@@ -20,6 +20,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet"
     >
+    <link rel="icon" href="{{  asset('favicon.ico') }}"
 
     @vite('resources/css/app.css')
     <link rel="stylesheet"
